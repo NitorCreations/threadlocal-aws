33b5d3aadf08c241b6d7ed2ea9f864273da90428
Pasi Niemi <pasi.niemi@nitor.com>
2019-07-04 14:20:42 +0300

    Small refactoring

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ import sys
 from setuptools import setup
 
 setup(name='threadlocal_aws',
-      version='0.3',
+      version='0.4',
       description='Library to access threadlocal aws clients and resources',
       url='http://github.com/NitorCreations/threadlocal-aws',
-      download_url='https://github.com/NitorCreations/threadlocal-aws/tarball/0.3',
+      download_url='https://github.com/NitorCreations/threadlocal-aws/tarball/0.4',
       author='Pasi Niemi',
       author_email='pasi.niemi@nitor.com',
       license='Apache 2.0',
