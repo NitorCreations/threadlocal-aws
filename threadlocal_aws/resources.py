2f8eafcfb18fca256b8f4149cff8f6086e7f09bb
Pasi Niemi <pasi.niemi@nitor.com>
2020-03-13 08:29:11 +0200

    Add optional c_ prefix for clients and r_ prefix for resources to work around e.g. lambda being a reserved word

--- threadlocal_aws/resources.py
+++ threadlocal_aws/resources.py
@@ -3,6 +3,8 @@ from threadlocal_aws import _get_local_resource, session, PY37
 from threadlocal_aws.pep562 import Pep562
 
 def __getattr__(name):
+    if name.startswith("r_"):
+        name = name[2:]
     return lambda **kwargs: _get_local_resource(name, **kwargs)
 
 if not PY37:
