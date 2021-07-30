59c00bbbabd3db1b129ca035c8534dc449fafaa8
Pasi Niemi <pasi.niemi@nitor.com>
2021-07-30 11:11:59 +0300

    Add handling for subresources. You can now for instance  and then do  and always call the same threadlocal table object

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ import sys
 from setuptools import setup
 
 setup(name='threadlocal-aws',
-      version='0.8',
+      version='0.9',
       description='Library to access threadlocal aws clients and resources',
       url='http://github.com/NitorCreations/threadlocal-aws',
-      download_url='https://github.com/NitorCreations/threadlocal-aws/tarball/0.8',
+      download_url='https://github.com/NitorCreations/threadlocal-aws/tarball/0.9',
       author='Pasi Niemi',
       author_email='pasi.niemi@nitor.com',
       license='Apache 2.0',
